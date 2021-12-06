6f8e57f9d04a097cfc0d6e2c9729702c288f8d9e
BassantSabrymohamed <93921067+BassantSabrymohamed@users.noreply.github.com>
2021-12-06 12:34:06 +0200

    mo

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -27,7 +27,6 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
         //finview
         registerEmail=findViewById(R.id.registerEmail);
         registerPass=findViewById(R.id.registerPass);
